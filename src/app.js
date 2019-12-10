4f34f8bd6c14261f5b667bf70ac0e7674f9c0ff4
RohitKMishra <rohitmishra1499769@gmail.com>
2019-12-10 16:08:08 +0530

    setup added for heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // directory setup to be exposed by web server to browser
 // define paths for Express config
@@ -106,6 +107,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen( 3000, ()=> {
-    console.log('Serving on port no. 3000')
+app.listen( port, ()=> {
+    console.log('Serving on port no. ' + port)
 })
